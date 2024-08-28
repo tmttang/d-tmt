57ba1134801e66c5034b40e5ce3ba7db16acf3d1
tmttang <tmtd.tang@gmail.com>
2024-08-28 11:52:18 -0700

    Updated Homepage

--- src/pages/home/components/HeroSection.jsx
+++ src/pages/home/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <MotionContainer>
       <div className='container mx-auto'>
-        <div className='flex flex-col items-center min-h-70vh justify-center gap-6 py-20'>
+        <div className='flex flex-col items-center min-h-70vh justify-center gap-6 py-20 px-5'>
           <MotionInner>
             <div className='text-black max-w-3xl h-auto text-center'>
               <h1>
@@ -22,6 +22,7 @@ const HeroSection = () => {
                 <p className='rounded-full bg-light-gray p-2'>Tailwind CSS</p>
                 <p className='rounded-full bg-light-gray p-2'>GSAP</p>
                 <p className='rounded-full bg-light-gray p-2'>Framer Motion</p>
+                <p className='rounded-full bg-light-gray p-2'>Figma</p>
               </div>
             </MotionInner>
           </div>
