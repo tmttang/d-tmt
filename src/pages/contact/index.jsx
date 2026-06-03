065c728c49b84ebc671c35d9fc2b0a887238ec62
tmttang <tmtd.tang@gmail.com>
2026-06-02 17:33:47 -0700

    update contact email

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
               </MotionInner>
               <MotionInner>
                 <h2 className='text-gray'>
-                  <a href='mailto:tmtd.tang@gmail.com'>Get in touch</a>.
+                  <a href='mailto:mantik.tang@d-tmt.com'>Get in touch</a>.
                 </h2>
               </MotionInner>
             </div>
